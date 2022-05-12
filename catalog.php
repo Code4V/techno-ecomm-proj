2021209b40421af44899c039d37b5a1b9ce3ae43
Jeanne Francis Rivas <vashypies@gmail.com>
2022-05-12 23:02:08 +0800

    Added container for mainbody

--- catalog.php
+++ catalog.php
@@ -16,21 +16,39 @@
 <body>
    
 <div class="main">
+
+    <!-- HEADER SECTION -->
     <div class="header-container">
         <div class="header-top">checktop</div>
         <div class="header-bottom">checkbot</div>
     </div>
     
-    <div class="flexbox-mainbody">
-        <div class="flexbox-item flex-categories">
-        flex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex category
-        flex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex category
-        flex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex category
+
+    <!-- MAIN BODY SECTION -->
+    <div class="flexbox-mainbody-container">
+        <div class="flexbox-mainbody">
+            <div class="flexbox-item flex-categories">
+            flex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex category
+            flex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex category
+            flex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex categoryflex category
+            </div>
+            <div class="flexbox-item flex-main-catalog">
+            flex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex main
+            flex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex main
+            flex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex main
+                <div class="flexbox-item flex-display-products">
+                    <div class="flexbox-item flex-products"></div>
+                    <div class="flexbox-item flex-products"></div>
+                    <div class="flexbox-item flex-products"></div>
+                    <div class="flexbox-item flex-products"></div>
+                </div>
+                
+            </div>
         </div>
-        <div class="flexbox-item flex-main-catalog">
-        flex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex main
-        flex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex main
-        flex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex mainflex main
+    </div>
+    <div class="footer-container">
+        <div class="footer-content-conatiner">
+
         </div>
     </div>
 </div>

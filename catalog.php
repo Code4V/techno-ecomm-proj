6b19d205387e9c48beeade55306740cef500bcf0
Jeanne Francis Rivas <vashypies@gmail.com>
2022-05-17 00:27:02 +0800

    Added container for categories and main catalog

--- catalog.php
+++ catalog.php
@@ -27,13 +27,15 @@
     <!-- MAIN BODY SECTION -->
     <div class="flexbox-mainbody-container">
         <div class="flexbox-mainbody">
-            <div class="flexbox-item flex-categories">
+            <div class="flexbox-item flex-categories-container">
                 <div class="flexbox-item categories-title">
                     <p>Categories</p>
                 </div>
             </div>
-            <div class="flexbox-item flex-main-catalog">
-                <p>Results</p>
+            <div class="flexbox-item flex-main-catalog-container">
+                    <div class="flexbox-item flex-main-catalog-title">
+                        <p>Results</p>
+                    </div>
                     <div class="flexbox-item flex-display-products">
                         <div class="flexbox-item flex-products"></div>
                         

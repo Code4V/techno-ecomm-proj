ea16372a8880252d563b03b8d4f8ad5f7516ef61
Jeanne Francis Rivas <vashypies@gmail.com>
2022-07-01 01:06:31 +0800

    Added pic

--- catalog.php
+++ catalog.php
@@ -5,10 +5,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BuzzWork: Posts</title>
+    <link rel="icon" href="images/bee.png">
 
     <link rel="stylesheet" href="catalog.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -36,25 +38,41 @@
                 <div class="side-nav-lists">
                     
                     <div class="list-style nav-home">
-                        <img src="images/home-3-512.png"><a href="#">Home</a></img> 
+                        <a href="main.php" target="_blank"><img src="images/home-3-512.png" >Home</a></img> 
                     </div>
                     <div class="list-style nav-creators">
-                        <img src="images/user-4-512.png"><a href="#">Creators</a></img> 
+                        <a href="#"><img src="images/user-4-512.png">Creators</a></img> 
                     </div>
                     <div class="list-style nav-create-post">
-                        <img src="images/beeswax-2-512.png"><a href="#">Create Post</a></img> 
+                        <a href="#"><img src="images/beeswax-2-512.png">Create Post</a></img> 
                     </div>
                     <div class="list-style nav-account">
-                        <img src="images/home-3-512.png"><a href="#">Account</a></img> 
+                        <a href="#"><img src="images/home-3-512.png">Account</a></img> 
                     </div>
                 </div>       
             <!-- MAIN LISTING  -->
             </div>
             <div class="main-posts">
-                <div class="main-listing-posts">
-                    <div class="posts-container">
-                        testing
+                <div class="flex-item main-listing-posts">
+                    <div class="flex-item posts-container">
+                        <div class="flex-item posts-header">
+                            <img src="images/user-4-512.png" alt="worker-picture" class="worker-pfp">
+                            <p>Jeanne Francis Rivas</p>
+                        </div>
+                        <div class="flex-item posts-content">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem perferendis eaque soluta ducimus deleniti porro praesentium qui explicabo ratione possimus, rerum mollitia officia, hic ullam illum atque molestias facere unde.</p>
+                        </div>
+                        <div class="flex-item posts-footer">
+                            <p>Lorem </p>
+                        </div>
+                        <div class="flex-item posts-highlight">
+                            <img src="images/programming.jpg" class="highlight-pic">
+                        </div>
                     </div>
+
+                    
+                    
+                    
                 </div>
             </div>
 
